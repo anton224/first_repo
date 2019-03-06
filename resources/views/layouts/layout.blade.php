5089eb1eaa6501d1f78ee93529f4cf7f8de27f67
anton <antonru142@gmail.com>
2019-03-06 23:00:01 +0200

    errors fixed

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -23,7 +23,7 @@
         @endif
         <div class="row" id = "row_bar">
             <div class="col-md-8 col-lg-8">
-                @if(count($errors > 0))
+                @if(count($errors) > 0)
                     @foreach($errors->all() as $error)
                         <div class="alert alert-danger">
                             {{$error}}
