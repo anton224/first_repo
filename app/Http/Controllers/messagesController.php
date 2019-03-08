fc3fb58212392531ce58eb191bf8ba6a9004ffb0
anton <antonru142@gmail.com>
2019-03-08 21:01:44 +0200

    Message model added

--- app/Http/Controllers/messagesController.php
+++ app/Http/Controllers/messagesController.php
@@ -3,14 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class messagesController extends Controller
 {
     public function submit(Request $request){
         $this->validate($request,[
             'name' => 'required',
-            'email' => 'required'
+            'email' => 'required',
+            'message' => 'required'
         ]);
-        return $request->input('name');
+
+        // Create new message
+        $message = new Message;
+        $message->name = $request->input('name');
+        $message->message = $request->input('message');
+        $message->email = $request->input('email');
+
+        // Save message
+        $message->save();
+
+        //Redirect
+        return redirect('/')->with('success','Message Sent');
     }
 }
